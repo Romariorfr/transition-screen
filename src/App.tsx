73ef9ebc5c966f71fd350df10ad119c33400a25a
Romariorfr <romarioferreirarezende@hotmail.com>
2023-07-23 01:24:37 -0300

    feat: projeto igual do tutorial replicado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, CSSProperties, useEffect } from 'react'
+import { useTransition, animated, AnimatedProps, useSpringRef } from '@react-spring/web'
+import styles from './styles.module.css'
 import './App.css';
 
+const pages: ((props: AnimatedProps<{ style: CSSProperties }>) => React.ReactElement)[] = [
+  ({ style }) => <animated.div style={{ ...style, background: 'lightpink' }}>EU</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>SOU</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>FODA!</animated.div>,
+]
+
 function App() {
+  const [index, set] = useState(0)
+  const onClick = () => set(state => (state + 1) % 3)
+  const transRef = useSpringRef()
+  const transitions = useTransition(index, {
+    ref: transRef,
+    keys: null,
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
+  useEffect(() => {
+    transRef.start()
+  }, [index])
   return (
-    <div>
-      Ola mundos
+    <div className={`flex fill ${styles.container}`} onClick={onClick}>
+      {transitions((style, i) => {
+        const Page = pages[i]
+        return <Page style={style} />
+      })}
     </div>
   );
 }
